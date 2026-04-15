e6d4638447f1a18fc8bc52d6438a96ec8624cfc6
Kuklev Pavel <lgk1144@gmail.com>
2026-04-15 16:00:36 +0300

    add ore

--- obj/logic.d
+++
@@ -1,6 +0,0 @@
-obj/logic.o: src/logic.c src/../include/glad/glad.h src/generator.h \
- src/render.h src/logic.h
-src/../include/glad/glad.h:
-src/generator.h:
-src/render.h:
-src/logic.h:
--- obj/texture.d
+++
@@ -1,4 +0,0 @@
-obj/texture.o: src/texture.c src/../include/glad/glad.h \
- src/../external/stb/stb_image.h
-src/../include/glad/glad.h:
-src/../external/stb/stb_image.h:
